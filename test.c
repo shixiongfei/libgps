d6df00ede4dd18c8d4ef5018c42cdda64cfde8ae
Jenson@Mac <jenson.sze@gmail.com>
2014-03-04 22:27:35 +0800

    Fixed test.c printf format string

--- test.c
+++ test.c
@@ -15,12 +15,12 @@ int main(int argc, char* argv[]) {
 		printf("GPS Begin: %s, End: %s. %i Points.\n", gps_test->start_time, gps_test->end_time, gps_test->number);
 
 		if ( p ) {
-			printf(" 2009-10-17 18:37:28 [At] (%Lf, %Lf, %Lf)\n", p->x, p->y, p->z);
+			printf(" 2009-10-17 18:37:28 [At] (%lf, %lf, %lf)\n", p->x, p->y, p->z);
 		}
 
 		p = gps_match(gps_test, "2009-08-17 23:33:08");
 		if ( p ) {
-			printf(" 2009-08-17 23:33:08 [At] (%Lf, %Lf, %Lf)\n", p->x, p->y, p->z);
+			printf(" 2009-08-17 23:33:08 [At] (%lf, %lf, %lf)\n", p->x, p->y, p->z);
 		}
 		printf("\n");
 
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
 			printf(" GPS Group Begin: %s, End: %s. %i Points.\n", g->start_time, g->end_time, g->number);
 
 			while ( p ) {
-				printf("  %s [At] (%Lf, %Lf, %Lf)\n", p->time, p->x, p->y, p->z);
+				printf("  %s [At] (%lf, %lf, %lf)\n", p->time, p->x, p->y, p->z);
 				p = p->next;
 			}
 
